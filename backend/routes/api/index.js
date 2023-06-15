118a6ae07731c6c47fa68c95aacc148606559f38
tristan <thuckabee.work@gmail.com>
2023-06-14 21:40:20 -0500

    Added User Authentication Express Middleware

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,4 +1,7 @@
 const router = require('express').Router();
+const { restoreUser } = require('../../utils/auth.js');
+
+router.use(restoreUser);
 
 router.get("/csrf/restore", (req, res) => {
   const csrfToken = req.csrfToken();
