6c2c75bb13a27107fa4323b0c7fb3e2b76729d73
tristan <thuckabee.work@gmail.com>
2023-06-17 15:35:09 -0500

    added event seeder

---
+++ backend/db/seeders/004-demo-events.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const env = process.env.NODE_ENV;
+let opts = { tableName: 'Events'};
+if (env === 'production') { opts.schema = process.env.SCHEMA }
+
+const events = [
+  {
+    groupId:1,
+    venueId:1,
+    name:'Test Event',
+    description:'Testing',
+    type:'Online',
+    startDate:'2024-01-01',
+    endDate:'2024-01-01'
+  }
+];
+const eventNames = events.map(event => event.name);
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert(opts, events, {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete(opts, eventNames, {});
+  }
+};
