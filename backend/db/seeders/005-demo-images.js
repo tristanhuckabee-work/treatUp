a775787c0c003712d40d00f92bcfce4cfbed388e
tristan <thuckabee.work@gmail.com>
2023-06-17 16:24:49 -0500

    added an image seed

--- backend/db/seeders/005-demo-images.js
+++ backend/db/seeders/005-demo-images.js
@@ -5,7 +5,13 @@ let opts = { tableName: 'Images' };
 if (env === 'production') { opts.schema = process.env.SCHEMA }
 
 const groupImages = [];
-const eventImages = [];
+const eventImages = [
+  {
+    url: 'https://res.cloudinary.com/dzsgront4/image/upload/v1686805183/pexels-iv%C3%A1n-rivero-1599469_upsanl.jpg',
+    imageableType:'Event',
+    imageableId:1
+  }
+];
 const images = [...groupImages, ...eventImages];
 
 module.exports = {
