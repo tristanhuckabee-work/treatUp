4d17d82d7d990db2b946f228ff9a968bdef16d71
tristan <thuckabee.work@gmail.com>
2023-06-19 17:19:31 -0500

    fixed typo in seeder

--- backend/db/seeders/007-demo-attendees.js
+++ backend/db/seeders/007-demo-attendees.js
@@ -5,17 +5,17 @@ let opts = { tableName: 'Attendees'};
 if (env === 'production') { opts.schema = process.env.SCHEMA }
 const attendees = [
   {
-    eventid:1,
+    eventId:1,
     userId:1,
     status:'Attending'
   },
   {
-    eventid:1,
+    eventId:1,
     userId:2,
     status:'Waitlisted'
   },
   {
-    eventid:1,
+    eventId:1,
     userId:3,
     status:'Pending'
   },
